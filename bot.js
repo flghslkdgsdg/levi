2db9c3974a3676fe39c92b86ff4160f5c5710c2a
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2019-03-02 00:51:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ const developers = ['457656120404606987'];
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setGame(`~`,'https://www.twitch.tv/Mdax')
-  client.user.setStatus("idle")
+  client.user.setStatus("dnd")
 });
 
 client.on('message', message => {
