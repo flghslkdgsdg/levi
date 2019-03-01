2d63a57a4bdce44557a015cfbe5aa41bc8a6fc82
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2019-03-02 00:53:29 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const developers = ['457656120404606987'];
+const developers = ['427611667631964180'];
 
 
 client.on('ready', () => {
@@ -13,17 +13,17 @@ client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!developers.includes(message.author.id)) return;
 
-if (message.content.startsWith('Mwt')) {
+if (message.content.startsWith('Awt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
   client.user.setStatus("idle")
     message.channel.send(` ** ${argresult} \ ** `)
 } else 
-if (message.content.startsWith('Mls')) {
+if (message.content.startsWith('Als')) {
 client.user.setActivity(argresult , {type:'LISTENING'});
   client.user.setStatus("idle")
     message.channel.send(` ** ${argresult} \ ** `)
 } else 
-if (message.content.startsWith('Mst')) {
+if (message.content.startsWith('Ast')) {
   client.user.setGame(argresult, "https://www.twitch.tv/Mdax");
   client.user.setStatus("idle")
    message.channel.send(` ** ${argresult} \ ** `)
@@ -33,7 +33,7 @@ if (message.content.startsWith('Mst')) {
 client.on('message', message => {
         if (!developers.includes(message.author.id)) return;
   if (message.content === '0..') {
-  let channel = client.channels.get('534474000949313546');
+  let channel = client.channels.get('496436880720789524');
 
   channel.join()
   .then(connection => console.log('Connected'))
