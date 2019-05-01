dce1388d32623473c9bfc64a2dcf4d8b69302826
flghslkdgsdg <43896154+flghslkdgsdg@users.noreply.github.com>
2019-05-01 14:52:11 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,17 +13,17 @@ client.on('message', message => {
   var argresult = message.content.split(` `).slice(1).join(' ');
     if (!developers.includes(message.author.id)) return;
 
-if (message.content.startsWith('Lwt')) {
+if (message.content.startsWith('.wt')) {
 client.user.setActivity(argresult, {type:'WATCHING'});
   client.user.setStatus("dnd")
     message.channel.send(` ** ${argresult} \ ** `)
 } else 
-if (message.content.startsWith('Lls')) {
+if (message.content.startsWith('.ls')) {
 client.user.setActivity(argresult , {type:'LISTENING'});
   client.user.setStatus("dnd")
     message.channel.send(` ** ${argresult} \ ** `)
 } else 
-if (message.content.startsWith('Lst')) {
+if (message.content.startsWith('.st')) {
   client.user.setGame(argresult, "https://www.twitch.tv/Mdax");
   client.user.setStatus("dnd")
    message.channel.send(` ** ${argresult} \ ** `)
@@ -33,7 +33,7 @@ if (message.content.startsWith('Lst')) {
 client.on('message', message => {
         if (!developers.includes(message.author.id)) return;
   if (message.content === '0..') {
-  let channel = client.channels.get('570908144801284116');
+  let channel = client.channels.get('496436880720789524');
 
   channel.join()
   .then(connection => console.log('Connected'))
